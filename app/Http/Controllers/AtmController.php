5f8738096c2bfc818e9ce7b2c2a9baf3c228d6da
kazuki-tokunaga <kazuki.tokunaga@fox-hound.jp>
2024-03-12 15:42:06 +0900

    500 Internal Server Error

--- app/Http/Controllers/AtmController.php
+++ app/Http/Controllers/AtmController.php
@@ -1,7 +1,21 @@
 <?php
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\BankAccount;
+
 class AtmController extends Controller
 {
+    public function createToken()
+    {
+        return csrf_field();
+        //NtdpjzoiGc0dVpegE7cgWm5zguKONjvT1GuxMCa4
+    }
 
+    public function accountOpen(Request $request)
+    {
+        $bankAccount = new bankAccounts();
+        $bankAccount->fill($request->all())->save;
+        return response()->json($bankAccount);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('createToken', 'App\Http\Controllers\AtmController@createToken');
+Route::post('bankTrading/accountOpening', 'App\Http\Controllers\AtmController@accountOpen');
